a50c8be18041f7157c8b84890487ad0e138a4683
= <shaankaranmathur@gmail.com>
2017-05-22 20:29:19 -0700

    Default conversion to Fahrenheit

--- Project4/Project4B/lab4b.c
+++ Project4/Project4B/lab4b.c
@@ -7,7 +7,10 @@
 const int B = 4275; // B value of the thermistor
 const int R0 = 100000; // R0 = 100k
 
-double  read_temperature(mraa_aio_context* temp){
+#define FAHR 0
+#define CELS 1
+
+double  read_temperature(mraa_aio_context* temp, int mode){
   if(*temp == NULL){
     fprintf(stderr, "MRAA structure NULL.\n");
     exit(1);
@@ -18,7 +21,11 @@ double  read_temperature(mraa_aio_context* temp){
   R = R0*R;
 
   double temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
-  
+
+  if(mode == FAHR){
+    temperature = temperature * 1.5 + 32;
+
+  }
   //float R = 1023.0/read - 1.0;
   //R *= R0;
 
@@ -38,7 +45,7 @@ void init(mraa_aio_context* t_sensor){
 
 int main(){
   unsigned int seconds = 1;  // COMMAND LINE ARGUMENT
-
+  int mode = FAHR;           // COMMAND LINE ARGUMENT
   mraa_aio_context t_sensor;
   init(&t_sensor);
 
@@ -50,7 +57,7 @@ int main(){
     time(&rawtime);
     timeinfo = localtime(&rawtime);
     
-    printf("%02d:%02d:%02d %.1f\n", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, read_temperature(&t_sensor));
+    printf("%02d:%02d:%02d %.1f\n", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, read_temperature(&t_sensor, mode));
     sleep(seconds);
   }
   
