304c28a0002814044e848b813457a8881ff0bca4
Garvit Pugalia <garvitpugalia@gmail.com>
2017-05-29 00:06:48 -0700

    Fixed number of blocks output for GROUP

--- Project3/Project3A/lab3a.cpp
+++ Project3/Project3A/lab3a.cpp
@@ -75,7 +75,13 @@ void getGroupDescriptor(EXT2_info &info){
   off_t offset = SUPERBLOCK_OFFSET + (EXT2_MIN_BLOCK_SIZE << info.super_block->s_log_block_size);
   ssize_t rc = Pread(info.image_fd, info.des_table, sizeof(ext2_group_desc), offset);
   stringstream ss;
-  ss << "GROUP,0," << info.super_block->s_blocks_per_group << "," << info.super_block->s_inodes_per_group << "," << info.des_table->bg_free_blocks_count << "," << info.des_table->bg_free_inodes_count << "," << info.des_table->bg_block_bitmap << "," << info.des_table->bg_inode_bitmap << "," << info.des_table->bg_inode_table;
+  ss << "GROUP,0," << info.super_block->s_blocks_count << ","
+     << info.super_block->s_inodes_count << ","
+     << info.des_table->bg_free_blocks_count << ","
+     << info.des_table->bg_free_inodes_count << ","
+     << info.des_table->bg_block_bitmap << ","
+     << info.des_table->bg_inode_bitmap << ","
+     << info.des_table->bg_inode_table << endl;
   Print(ss.str());
 }
 
