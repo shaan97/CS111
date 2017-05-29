23a6081669ac8171d9a72a75ade01961107def13
= <shaankaranmathur@gmail.com>
2017-05-28 19:38:13 -0700

    Fixed preprocessor directives and included Ext2_fs.h wrapper

--- Project3/Project3A/lab3a.h
+++ Project3/Project3A/lab3a.h
@@ -1,8 +1,14 @@
-#IFNDEF LAB3A_H
-#DEFINE LAB3A_H
+#ifndef LAB3A_H
+#define LAB3A_H
+
+#include "Ext2_fs.h" // Wrapper for ext2_fs.h
+#include <string>
+#include "EXT2_info.h"
 
 #define SUPERBLOCK_OFFSET 0x400
 enum FILE_DATA = { SUPERBLOCK, GROUP, BFREE, IFREE, INODE, DIRENT, INDIRECT};
 void getSuperblock(EXT2_info* info);
 void getGroupDescriptor(EXT2_info* info);
 void Print(FILE_DATA type, std::string message);
+
+#endif
