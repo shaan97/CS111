d441ae3019f9b5b1de52a5222b147254c6002368
= <shaankaranmathur@gmail.com>
2017-05-29 00:51:25 -0700

    Added getFreeBlock(...) and getFreeInode(...) declarations as well added const& specifiers to Print(...) and getInode(...)

--- Project3/Project3A/lab3a.h
+++ Project3/Project3A/lab3a.h
@@ -11,10 +11,10 @@
 #define SUPERBLOCK_OFFSET 0x400
 void getSuperblock(EXT2_info &info);
 void getGroupDescriptor(EXT2_info &info);
-void Print(std::string message);
-void readImap(const EXT2_info &info);
-void readDmap(const EXT2_info &info);
-void readInode(const EXT2_info &info);
+void getFreeBlock(const EXT2_info &info);
+void getFreeInode(const EXT2_info &info);
+void Print(const std::string message);
+void getInode(const EXT2_info &info);
 ssize_t Pread(int fd, void *buf, size_t count, off_t offset);
 void process_dir();
 
