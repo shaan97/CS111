8c7d7c05813e8044c3e576dd9bac8876a14ca3bc
Garvit Pugalia <garvitpugalia@gmail.com>
2017-05-28 19:40:55 -0700

    Added Pread wrapper

--- Project3/Project3A/lab3a.h
+++ Project3/Project3A/lab3a.h
@@ -2,12 +2,18 @@
 #define LAB3A_H
 
 #include <string>
+#include <unistd.h>
 #include "ext2_fs.h"
 
 #define SUPERBLOCK_OFFSET 0x400
-void getSuperblock(EXT2_info* info);
-void getGroupDescriptor(EXT2_info* info);
+void getSuperblock(EXT2_info &info);
+void getGroupDescriptor(EXT2_info &info);
 void Print(std::string message);
-void get
+void readImap(const EXT2_info &info);
+void readDmap(const EXT2_info &info);
+void readInode(const EXT2_info &info);
+ssize_t Pread(int fd, void *buf, size_t count, off_t offset);
+void process_dir();
+
 
 #endif
