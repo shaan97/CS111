7178dd4141b51cdc9476f408d3fbc54c4a2904fb
= <shaankaranmathur@gmail.com>
2017-06-03 20:34:27 -0700

    Changed indentation from spaces to tabs.

--- Project3/Project3B/data.h
+++ Project3/Project3B/data.h
@@ -5,38 +5,38 @@
 #include <unordered_map>
 
 struct Inode {
-    bool isAllocated = false;
-    bool onFreeList = false;
-    int linkcount = 0;
-    int numLinks = 0;
+	bool isAllocated = false;
+	bool onFreeList = false;
+	int linkcount = 0;
+	int numLinks = 0;
 };
 
 struct Indirect {
-    long offset;
-    long level;
-    long inode;
+	long offset;
+	long level;
+	long inode;
 };
 
 struct Group {
-    long num;
-    long freeBlocks;
-    long firstBlock;
+	long num;
+	long freeBlocks;
+	long firstBlock;
 };
 
 struct SuperBlock {
-    long numBlocks;
-    long numInodes;
-    long nonreserved_inode;
-    long inodeSize = -1;
-    long blockSize;
-    long inodesPerGroup;
+	long numBlocks;
+	long numInodes;
+	long nonreserved_inode;
+	long inodeSize = -1;
+	long blockSize;
+	long inodesPerGroup;
 };
 
 struct Block {
-    std::vector<long> inodes;
-    std::vector<long> offsets;
-    bool onFreelist = false;
-    std::vector<int> levels;
+	std::vector<long> inodes;
+	std::vector<long> offsets;
+	bool onFreelist = false;
+	std::vector<int> levels;
 };
 
 
